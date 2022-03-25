b5d708407d5c586d6610c9fbf6e459b36fad32b8
Felix-Neko <felix-neko@list.ru>
2022-03-25 15:38:57 +0300

    Some args passed...

--- airflow_sandbox/dags/simple_python_dag_2.py
+++ airflow_sandbox/dags/simple_python_dag_2.py
@@ -10,7 +10,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.executors.debug_executor import DebugExecutor
 
-# Я знаю, что сейчас 2k22 год, и надо работать через контексты и декораторы. Это специально для простоты.
+# Я знаю, что сейчас 2k22 год, и надо работать через контексты и декораторы. Это специально для простоты отладки.
 dag = DAG(
     dag_id='simple_python_dag_2',
     schedule_interval=None,
@@ -21,16 +21,19 @@ dag = DAG(
 
 # Вот эта функция печатает всё в логи при запуске через GUI Airflow,
 # но ни хрена не печатает при запуске через python3 simply_python_dag.py
-def hello_world():
+def hello_world(*args, **kwargs):
     print("=================")
     print("Hello world!!!")
     print("=================")
+    print(args)
+    print(kwargs)
+    print("=================")
 
-hello_world_operator = PythonOperator(dag=dag, task_id="hello_world", python_callable=hello_world)
+hello_world_operator = PythonOperator(dag=dag, task_id="hello_world", python_callable=hello_world, op_args=[1, 2, 3])
 
 
 if __name__ == "__main__":
     dag.clear()
     # лол, чтобы брякпоинты заработали (и чтобы принты заработали), нало запускать с новой датой...
-    dag.run(start_date=pendulum.now(), executor=DebugExecutor(),
+    dag.run(start_date=pendulum.datetime(2021, 5, 1, tz="Europe/Moscow"), end_date=pendulum.now("Europe/Moscow"), executor=DebugExecutor(),
             run_at_least_once=True)
