e859d6d70a209219504a9447639b4c6e356549a1
Алексей Головизин <agolovizin@alfabank.ru>
2022-06-19 14:55:08 +0300

    Okay, somehow runnin' some python...

---
+++ kube_client_fuss/kube_client_fuss_1.py
@@ -0,0 +1,16 @@
+from kubernetes import client, config, utils
+from devtools import debug
+
+config.load_kube_config()  # for local environment
+
+k8s_client = client.ApiClient()
+
+utils.create_from_yaml()
+
+v1 = client.CoreV1Api()
+
+# nodes_info = v1.list_node()
+
+ret = v1.list_pod_for_all_namespaces(watch=False)
+for i in ret.items:
+    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
---
+++ kube_client_fuss/mount_data.sh
@@ -0,0 +1,3 @@
+BASEDIR=$(dirname "$0")
+
+minikube mount $BASEDIR/data:/mnt/data
