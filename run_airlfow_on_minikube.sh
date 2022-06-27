121e8afc4858be9ae4c5089d61a061fc77cd6dbf
Алексей Головизин <agolovizin@alfabank.ru>
2022-06-27 12:29:37 +0300

    Minor cleanup

--- run_airlfow_on_minikube.sh
+++ run_airlfow_on_minikube.sh
@@ -9,4 +9,6 @@ kubectl apply -f host_dags_pv.yaml
 kubectl apply -f host_dags_pvc.yaml
 
 #helm upgrade --install airflow apache-airflow/airflow --set dags.persistence.enabled=true --set dags.persistence.existingClaim=host-dags-pvc --set dags.gitSync.enabled=false --set executor=KubernetesExecutor --set logs.persistence.enabled=true --set image.tag=latest-python3.7
+
+helm repo add airflow-stable https://airflow-helm.github.io/charts
 helm upgrade airflow airflow-stable/airflow --install -f values.yaml
