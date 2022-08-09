945bdbd80324e3bf17674c2808786ca18e8d8f6f
Алексей Головизин <agolovizin@alfabank.ru>
2022-08-09 13:05:27 +0300

    Hadoop configs added...

--- docker/airflow_docker_image/build_and_push.sh
+++ docker/airflow_docker_image/build_and_push.sh
@@ -1,2 +1,2 @@
-docker build . -t mlops-docker-snapshots.binary.alfabank.ru/airflow_fs_etl:latest --network host --no-cache
+docker build . -t mlops-docker-snapshots.binary.alfabank.ru/airflow_fs_etl:latest --network host
 docker push mlops-docker-snapshots.binary.alfabank.ru/airflow_fs_etl:latest
