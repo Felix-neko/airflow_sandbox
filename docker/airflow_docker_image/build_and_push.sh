efb61bda735a7513d7fcae69243405fdf5bf2068
Алексей Головизин <agolovizin@alfabank.ru>
2022-08-10 22:09:13 +0300

    Dockerfile updated...

--- docker/airflow_docker_image/build_and_push.sh
+++ docker/airflow_docker_image/build_and_push.sh
@@ -1,2 +1,2 @@
-docker build . -t mlops-docker-snapshots.binary.alfabank.ru/fs_etl_basic:v0.3 --network host --no-cache
-docker push mlops-docker-snapshots.binary.alfabank.ru/fs_etl_basic:v0.3
+docker build . -t mlops-docker-snapshots.binary.alfabank.ru/airflow_fs_etl:v0.6.7 --network host --no-cache
+docker push mlops-docker-snapshots.binary.alfabank.ru/airflow_fs_etl:v0.6.7
